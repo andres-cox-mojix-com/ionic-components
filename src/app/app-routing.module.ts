a47b40dd74036133139cb63fea1f76de743cf9c5
andres-cox-mojix-com <andreshcox@gmail.com>
2019-03-20 13:19:26 -0400

    added check, date, fab, grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   { path: 'avatar', loadChildren: './pages/avatar/avatar.module#AvatarPageModule' },
   { path: 'buttons', loadChildren: './pages/buttons/buttons.module#ButtonsPageModule' },
   { path: 'card', loadChildren: './pages/card/card.module#CardPageModule' },
+  { path: 'check', loadChildren: './pages/check/check.module#CheckPageModule' },
+  { path: 'date-time', loadChildren: './pages/date-time/date-time.module#DateTimePageModule' },
+  { path: 'fab', loadChildren: './pages/fab/fab.module#FabPageModule' },
+  { path: 'grid', loadChildren: './pages/grid/grid.module#GridPageModule' },
 
 ];
 
